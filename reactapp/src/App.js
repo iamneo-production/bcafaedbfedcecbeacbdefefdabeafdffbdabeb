f15089853eeac883100883d00bb749f54fbbc0cc
Iamneo <techadmin@iamneo.ai>
2023-09-01 07:13:18 +0000

    Backup Commit

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -2,15 +2,15 @@ import { filterOdd, filterEven, generateRandomList } from './functions/filterOdd
 import './App.css';
 function App() {
   const randomList = generateRandomList();
-  const oddList = filterOdd(...randomList);
-  const evenList = filterEven(...randomList);
+  const oddList = filterOdd(...randomList).join(',');
+  const evenList = filterEven(...randomList).join(',');
 
   return (
     <div>
       <h1><u>FILTER ODD EVEN</u></h1>
       <h2 id="d1">Random List: {randomList.join(', ')}</h2>
-      <h2 id="d2">Odd List: {oddList.join(', ')}</h2>
-      <h2 id="d3">Even List: {evenList.join(', ')}</h2>
+      <h2 id="d2">Odd List: {oddList}</h2>
+      <h2 id="d3">Even List: {evenList}</h2>
     </div>
   );
 }
